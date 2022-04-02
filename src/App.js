f701ee357bb2b6859f2bbb623573a74f2d1d8cc6
TinasheCalvin <makaratinashe22@gmail.com>
2022-04-03 00:14:38 +0200

    setup dev environment to kickstart website development

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
+import './app.scss';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        App
+      </div>
+      <div className="bottom">
+        Bottom
+      </div>
+    </>
   );
 }
 
