c1672f8aa0e2121d321f59e5ebb9f5bc1cf92e93
TinasheCalvin <makaratinashe22@gmail.com>
2022-04-15 22:52:53 +0200

    completed pricing component

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -55,6 +55,75 @@ function Pricing() {
                 </div>
             </div>
             <h1 className="title">Add Ons</h1>
+            <div className="additionals">
+                <div className="addition">
+                    <div className="addon">Fresh Store</div>
+                    <div className="addon-price">
+                        <span>$49</span> /month paid quarterly
+                    </div>
+                    <p className="addon-desc">
+                        Get the best tools for easy order management for your restaurant
+                    </p>
+                    <a href="" className="link">
+                        <span className="text">Buy Kitchen App </span><ArrowForwardRounded className='icon'/>
+                    </a>
+                </div>
+                <div className="addition">
+                    <div className="addon">SMS Notification</div>
+                    <div className="addon-price">
+                        <span>$49</span> /month paid quarterly
+                    </div>
+                    <p className="addon-desc">
+                        Get faster notifications via SMS each time an order comes in.
+                    </p>
+                    <a href="" className="link">
+                        <span className="text">Buy SMS Notification </span><ArrowForwardRounded className='icon'/>
+                    </a>
+                </div>
+                <div className="addition">
+                    <div className="addon">Fresh Logistics</div>
+                    <div className="addon-price">
+                        <span>$4.99</span> /delivery
+                    </div>
+                    <p className="addon-desc">
+                        Get your orders delivered by our experienced drivers.
+                    </p>
+                    <a href="" className="link">
+                        <span className="text">Buy Fresh Logistics </span><ArrowForwardRounded className='icon'/>
+                    </a>
+                </div>
+                <div className="addition">
+                    <div className="addon">Android App</div>
+                    <div className="addon-price">
+                        <span>$49</span> /month paid quarterly
+                    </div>
+                    <p className="addon-desc">
+                        Reach a lot more customers with your app on the Google Play Store.
+                    </p>
+                    <a href="" className="link">
+                        <span className="text">Buy Android App </span><ArrowForwardRounded className='icon'/>
+                    </a>
+                </div>
+                <div className="addition">
+                    <div className="addon">IOS App</div>
+                    <div className="addon-price">
+                        <span>$49</span> /month paid quarterly
+                    </div>
+                    <p className="addon-desc">
+                        Faster Browsing, App Notifications {'&'} other benefits of a native IOS App.
+                    </p>
+                    <a href="" className="link">
+                        <span className="text">Buy IOS App </span><ArrowForwardRounded className='icon'/>
+                    </a>
+                </div>
+            </div>
+            <div className="contact">
+                <a href="mailto:hello@freshideas.co.zw?subject=Interested%20in%20the%20Standard%20Medium%20Store%20Package" className="link">
+                    <span className="text">Get in touch with us </span><ArrowForwardRounded className='icon'/>
+                </a>
+                <p>Talk to us at</p>
+                <a href="" className='email'>hello@freshideas.co.zw</a>
+            </div>
         </div>
     )
 }
